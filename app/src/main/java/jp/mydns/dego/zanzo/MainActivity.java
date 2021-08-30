fd63e7af3f870251c1276e2d29fc849ad4d801a7
dego <dego.project@gmail.com>
2021-08-30 10:41:17 +0900

    画面回転時の一時停止処理を修正

--- app/src/main/java/jp/mydns/dego/zanzo/MainActivity.java
+++ app/src/main/java/jp/mydns/dego/zanzo/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends Activity {
     public void onStop() {
         DebugLog.d(TAG, "onStop");
         super.onStop();
-        this.getVideoController().pause();
+        this.getVideoController().join(true);
     }
 
     /**
